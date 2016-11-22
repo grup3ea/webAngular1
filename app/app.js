e4795a827fd08e35592689afabe90468e3c308e7
lima9 <max9@openmailbox.org>
2016-11-22 10:27:51 +0100

    Hay una movida importante con los views, la funcion de users no se muestra y no se porque, todo lo demas funciona

--- app/app.js
+++ app/app.js
@@ -1,20 +1,20 @@
 'use strict';
 
 
-var urlapi="http://localhost:3005/api/";
+var urlapi = "http://localhost:3005/api/";
 //var urlapi="http://147.83.7.158:3005/api/";
 
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
-  'ngRoute',
-  'myApp.main',
-  'myApp.login',
-  'myApp.signup',
-  'myApp.dashboard',
-  'myApp.users'
-]).
-config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-  $locationProvider.hashPrefix('!');
+    'ngRoute',
+    'myApp.main',
+    'myApp.login',
+    'myApp.signup',
+    'myApp.dashboard',
+    'myApp.users',
+    'myApp.trainers'
+]).config(['$locationProvider', '$routeProvider', function ($locationProvider, $routeProvider) {
+    $locationProvider.hashPrefix('!');
 
-  $routeProvider.otherwise({redirectTo: '/main'});
+    $routeProvider.otherwise({redirectTo: '/main'});
 }]);
--- app/views/trainers/trainers.js
+++ app/views/trainers/trainers.js
@@ -1,3 +1,35 @@
-/**
- * Created by ns on 21/11/16.
- */
+'use strict';
+
+angular.module('myApp.trainers', ['ngRoute'])
+
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/trainer', {
+            templateUrl: 'views/users/trainers.html',
+            controller: 'TrainersCtrl'
+        });
+    }])
+
+    .controller('TrainersCtrl', function ($scope, $http) {
+        if (localStorage.getItem('fs_web_token')) {// adding token to the headers
+            $http.defaults.headers.post['X-Access-Token'] = localStorage.getItem('fs_web_token');
+            //el .common serveix per als gets
+            $http.defaults.headers.common['X-Access-Token'] = localStorage.getItem('fs_web_token');
+        }
+        $scope.storageuser = JSON.parse(localStorage.getItem("fs_web_trainerdata"));
+        $scope.users = {};
+        $http.get(urlapi + 'users/trainers')
+            .success(function (data) {
+                console.log('data success');
+                console.log(data); // for browser console
+                $scope.users = data; // for UI
+                localStorage.setItem('fs_web_users', JSON.stringify($scope.users));
+            })
+            .error(function (data, status) {
+                console.log('data error');
+                console.log(status);
+                console.log(data);
+            })
+            .then(function (result) {
+                //users = result.data;
+            });
+    });
