7c24512bd510a6eaafbc78792dfd9aec8e047812
lima9 <max9@openmailbox.org>
2016-12-12 09:52:45 +0100

    Modified main.js

--- app/views/main/main.js
+++ app/views/main/main.js
@@ -1,5 +1,5 @@
 'use strict';
-angular.module('myApp.main', ['ngRoute','ngMaterial'])
+angular.module('myApp.main', ['ngRoute', 'ngMaterial'])
     .config(['$routeProvider', function ($routeProvider) {
         $routeProvider.when('/main', {
             templateUrl: 'views/main/main.html',
@@ -7,6 +7,12 @@ angular.module('myApp.main', ['ngRoute','ngMaterial'])
         });
     }])
     .controller('MainCtrl', function ($scope, $http, $timeout, $window) {
+        if (localStorage.getItem('fs_web_token')) {// adding token to the headers
+            $http.defaults.headers.post['X-Access-Token'] = localStorage.getItem('fs_web_token');
+            //el .common serveix per als gets
+            $http.defaults.headers.common['X-Access-Token'] = localStorage.getItem('fs_web_token');
+
+        }
         $scope.loginData = {};
         $scope.logout = function () {
             localStorage.removeItem("fs_web_userdata");
@@ -14,4 +20,19 @@ angular.module('myApp.main', ['ngRoute','ngMaterial'])
                 $window.location.reload(true);
             }, 1000);
         };
+        $scope.trainers = {};
+        $http.get(urlapi + '/trainers')
+            .success(function (data) {
+                console.log('data success');
+                console.log(data); // for browser console
+                $scope.trainers = data; // for UI
+                localStorage.setItem('fs_web_trainers', JSON.stringify($scope.trainers));
+            })
+            .error(function (data, status) {
+                console.log('data error');
+                console.log(status);
+                console.log(data);
+            })
+            .then(function (result) {
+            });
     });
