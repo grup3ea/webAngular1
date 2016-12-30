02f16d2f8f068830b0ca1c6dae1e69ad3bc33a23
arnaucode <idoctnef@openmailbox.org>
2016-12-30 13:50:18 +0100

    implementat el pujar imatges dels meals al cdn extern

--- app/views/editDiet/editDiet.js
+++ app/views/editDiet/editDiet.js
@@ -98,4 +98,41 @@ angular.module('myApp.editDiet', ['ngRoute', 'ngFileUpload'])
                     });
         };
 
+
+                /* cloudinary */
+                  $scope.uploadFile = function(file, index){
+                    console.log(index);
+                    var d = new Date();
+                    $scope.title = "Image (" + d.getDate() + " - " + d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds() + ")";
+
+                    $scope.meals[index].imgfile = file;
+                    if (!$scope.meals[index].imgfile) return;
+                      if (file && !file.$error) {
+                        console.log(file);
+                        file.upload = Upload.upload({
+                          url: "https://api.cloudinary.com/v1_1/" + cloudinary.config().cloud_name + "/upload",
+                          data: {
+                            upload_preset: cloudinary.config().upload_preset,
+                            tags: 'myphotoalbum',
+                            context: 'photo=' + $scope.title,
+                            file: file
+                          },
+                          headers: {
+                           'X-Access-Token': undefined
+                          },
+                        }).progress(function (e) {
+                          file.progress = Math.round((e.loaded * 100.0) / e.total);
+                          file.status = "Uploading... " + file.progress + "%";
+                        }).success(function (data, status, headers, config) {
+                          console.log(data.url);
+                          $scope.meals[index].img=data.url;
+                          $rootScope.photos = $rootScope.photos || [];
+                          data.context = {custom: {photo: $scope.title}};
+                          file.result = data;
+                          $rootScope.photos.push(data);
+                        }).error(function (data, status, headers, config) {
+                          file.result = data;
+                        });
+                      }
+                  };/* end of cloudinary */
     });
