124b3fcbb5bdf2318a2575883591fbcbc6763ca9
lima9 <max9@openmailbox.org>
2017-01-01 18:40:10 +0100

    Added IP

--- app/views/login/login.js
+++ app/views/login/login.js
@@ -7,9 +7,14 @@ angular.module('myApp.login', ['ngRoute', 'ng.deviceDetector'])
         });
     }])
     .controller('LoginCtrl', function ($scope, $http, $timeout, $window, $mdToast, deviceDetector) {
-      var vm = this;
-      vm.data = deviceDetector;
-      console.log("userAgent: " + vm.data.raw.userAgent);
+        var vm = this;
+        vm.data = deviceDetector;
+        var ip = $.getJSON('//freegeoip.net/json/?callback=?', function(data) {
+            ip = JSON.stringify(data);
+            console.log(ip);
+        });
+
+        console.log("userAgent: " + vm.data.raw.userAgent);
         $scope.loginData = {};
         $scope.doLogin = function () {
             console.log('Doing login', $scope.loginData);
@@ -18,43 +23,44 @@ angular.module('myApp.login', ['ngRoute', 'ng.deviceDetector'])
             $scope.loginData.browser=vm.data.raw.browser;
             $scope.loginData.device=vm.data.raw.device;
             $scope.loginData.os_version=vm.data.raw.os_version;
+            $scope.loginData.ip = ip;
             $scope.loginData.browser_version=vm.data.raw.browser_version;
             $http({
                 url: urlapi + $scope.loginData.role+ 's/login',
                 method: "POST",
                 data: $scope.loginData
             })
-            .then(function (response) {
-            // success
-            console.log("response: ");
-            console.log(response.data);
-            if (response.data.success == true) {
-                localStorage.setItem("fs_web_token", response.data.token);
-                localStorage.setItem("fs_web_userdata", JSON.stringify(response.data.user));
-                window.location.reload();
-                /*$timeout(function () {
-                    $window.location="#!/dashboard"; //peta pq fa la petició un cop al dashboard sense afegir el token, pq no ha afegit els httpProviders
-                }, 1000);*/
-            } else {
-                console.log("login failed");
-                $mdToast.show(
-                   $mdToast.simple()
-                      .textContent('Account not found')
-                      .position("bottom right")
-                      .hideDelay(3000)
-                );
-                //$ionicLoading.show({ template: 'Login failed, user or password error.', noBackdrop: true, duration: 2000 });
-            }
-          },
-          function (response) { // optional
-              // failed
-              console.log(response);
-          });
+                .then(function (response) {
+                        // success
+                        console.log("response: ");
+                        console.log(response.data);
+                        if (response.data.success == true) {
+                            localStorage.setItem("fs_web_token", response.data.token);
+                            localStorage.setItem("fs_web_userdata", JSON.stringify(response.data.user));
+                            window.location.reload();
+                            /*$timeout(function () {
+                             $window.location="#!/dashboard"; //peta pq fa la petició un cop al dashboard sense afegir el token, pq no ha afegit els httpProviders
+                             }, 1000);*/
+                        } else {
+                            console.log("login failed");
+                            $mdToast.show(
+                                $mdToast.simple()
+                                    .textContent('Account not found')
+                                    .position("bottom right")
+                                    .hideDelay(3000)
+                            );
+                            //$ionicLoading.show({ template: 'Login failed, user or password error.', noBackdrop: true, duration: 2000 });
+                        }
+                    },
+                    function (response) { // optional
+                        // failed
+                        console.log(response);
+                    });
         };/*
-        $scope.logout = function () {
-            localStorage.removeItem("fs_web_userdata");
-            $timeout(function () {
-                $window.location.reload(true);
-            }, 1000);
-        };*/
+         $scope.logout = function () {
+         localStorage.removeItem("fs_web_userdata");
+         $timeout(function () {
+         $window.location.reload(true);
+         }, 1000);
+         };*/
     });
