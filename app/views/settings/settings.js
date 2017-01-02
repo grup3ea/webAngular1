caa916c6fd2df5aacc114ab2b18e97bad89fb8a3
arnaucode <idoctnef@openmailbox.org>
2017-01-02 16:17:50 +0100

    implementat que pugui delete active sessions des de l'apartat de settings (exceptuant la sessió pròpia de la web)

--- app/views/settings/settings.js
+++ app/views/settings/settings.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('myApp.settings', ['ngRoute'])
+angular.module('myApp.settings', ['ngRoute', 'ng.deviceDetector'])
 
     .config(['$routeProvider', function ($routeProvider) {
         $routeProvider.when('/settings', {
@@ -9,8 +9,12 @@ angular.module('myApp.settings', ['ngRoute'])
         });
     }])
 
-    .controller('SettingsCtrl', function ($scope, $http) {
+    .controller('SettingsCtrl', function ($scope, $http, deviceDetector) {
       $scope.storageuser = JSON.parse(localStorage.getItem("fs_web_userdata"));
+      var vm = this;
+      vm.data = deviceDetector;
+      $scope.currentSession= vm.data;
+
       $http.get(urlapi + 'users/'+ $scope.storageuser._id)
         .then(function (data) {
             console.log('data success');
@@ -23,8 +27,33 @@ angular.module('myApp.settings', ['ngRoute'])
             console.log('data error');
             console.log(status);
             console.log(data);
-        })
-        .then(function (result) {
-            //users = result.data;
         });
+
+        $scope.deleteSelectedSessions = function(){
+          //aqí el post
+          var arrayOfDevicesToDelete=[];
+          for(var i=0; i<$scope.storageuser.tokens.length; i++)
+          {
+            if($scope.storageuser.tokens[i].todelete==true)
+            {
+              arrayOfDevicesToDelete.push($scope.storageuser.tokens[i]);
+            }
+          }
+          console.log(arrayOfDevicesToDelete);
+          $http({
+              url: urlapi + $scope.storageuser.role + 's/'+ $scope.storageuser._id + "/deleteSelectedTokens",
+              method: "POST",
+              data: {devicesToDelete: arrayOfDevicesToDelete}
+          })
+          .then(function (data) {
+                  // success
+                  console.log(data);
+                  localStorage.setItem("fs_web_userdata", JSON.stringify(data.data));
+                  $scope.storageuser=data.data;
+              },
+              function (data) { // optional
+                  // failed
+                  console.log(data);
+              });
+        };
     });
