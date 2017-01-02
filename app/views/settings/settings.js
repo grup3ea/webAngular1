47107a9ca3ee5c48342243dfde5af3b6c975eb62
arnaucode <idoctnef@openmailbox.org>
2017-01-02 15:06:01 +0100

    que mostri més info dels devices loguejats a setings

--- app/views/settings/settings.js
+++ app/views/settings/settings.js
@@ -10,5 +10,21 @@ angular.module('myApp.settings', ['ngRoute'])
     }])
 
     .controller('SettingsCtrl', function ($scope, $http) {
-        
+      $scope.storageuser = JSON.parse(localStorage.getItem("fs_web_userdata"));
+      $http.get(urlapi + 'users/'+ $scope.storageuser._id)
+        .then(function (data) {
+            console.log('data success');
+            console.log(data); // for browser console
+            //$scope.trainers = data.data; // for UI
+            //localStorage.setItem('fs_web_trainers', JSON.stringify($scope.trainers));
+            localStorage.setItem("fs_web_userdata", JSON.stringify(data.data));
+            $scope.storageuser=data.data;
+        }, function (data, status) {
+            console.log('data error');
+            console.log(status);
+            console.log(data);
+        })
+        .then(function (result) {
+            //users = result.data;
+        });
     });
