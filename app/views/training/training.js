1b275018a8e7feb08a9938de25ae8837bf991cfd
arnaucode <idoctnef@openmailbox.org>
2017-01-11 17:25:03 +0100

    improved training page

--- app/views/training/training.js
+++ app/views/training/training.js
@@ -2,14 +2,14 @@
 
 angular.module('myApp.training', ['ngRoute'])
 
-    .config(['$routeProvider', function ($routeProvider) {
+    .config(['$routeProvider', function($routeProvider) {
         $routeProvider.when('/training', {
             templateUrl: 'views/training/training.html',
             controller: 'TrainingCtrl'
         });
     }])
 
-    .controller('TrainingCtrl', function ($scope, $http) {
+    .controller('TrainingCtrl', function($scope, $http) {
         /*if (localStorage.getItem('fs_web_token')) {// adding token to the headers
             $http.defaults.headers.post['X-Access-Token'] = localStorage.getItem('fs_web_token');
             //el .common serveix per als gets
@@ -19,17 +19,31 @@ angular.module('myApp.training', ['ngRoute'])
 
         $scope.trainers = {};
         $http.get(urlapi + 'trainers')
-            .then(function (data) {
+            .then(function(data) {
                 console.log('data success');
                 console.log(data); // for browser console
                 $scope.trainers = data.data; // for UI
-                localStorage.setItem('fs_web_trainers', JSON.stringify($scope.trainers));
-            }, function (data, status) {
+            }, function(data, status) {
                 console.log('data error');
                 console.log(status);
                 console.log(data);
             })
-            .then(function (result) {
+            .then(function(result) {
                 //users = result.data;
             });
+        $http.get(urlapi + 'users/' + $scope.storageuser._id)
+            .then(function(data) {
+                console.log('data success');
+                console.log(data); // for browser console
+                $scope.user = data.data; // for UI
+                if ($scope.user._id == $scope.storageuser._id) {
+                    localStorage.setItem("fs_web_userdata", JSON.stringify($scope.user));
+                    $scope.storageuser = JSON.parse(localStorage.getItem("fs_web_userdata"));
+                }
+            }, function(data, status) {
+                console.log('data error');
+                console.log(status);
+                console.log(data);
+            })
+            .then(function(result) {});
     });
