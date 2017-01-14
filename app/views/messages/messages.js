0d2ce82017be577483f2c873be9ed8b92943796b
arnaucode <idoctnef@openmailbox.org>
2017-01-14 22:45:58 +0100

    implementat send message to conversation

--- app/views/messages/messages.js
+++ app/views/messages/messages.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('myApp.messages', ['ngRoute', 'chart.js'])
+angular.module('myApp.messages', ['ngRoute', 'chart.js', 'ngAnimate', 'toastr', 'luegg.directives'])
 
     .config(['$routeProvider', function ($routeProvider) {
         $routeProvider.when('/messages', {
@@ -9,14 +9,46 @@ angular.module('myApp.messages', ['ngRoute', 'chart.js'])
         });
     }])
 
-    .controller('MessagesCtrl', function ($scope, $http, $routeParams, $filter) {
+    .controller('MessagesCtrl', function ($scope, $http, $routeParams, $filter,
+                                    toastr, $route) {
         $scope.storageuser = JSON.parse(localStorage.getItem("fs_web_userdata"));
-        $scope.conversations = {};
-        $http.get(urlapi + 'conversations')
+
+        $scope.selectedConversation={};
+        $scope.indexSelectedConversation;
+        $scope.selectConversation=function(conv, indexConv){
+            console.log(conv);
+            $scope.selectedConversation=conv;
+            $scope.indexSelectedConversation=indexConv;
+        };
+
+        $scope.sendMessage = function(){
+            $http({
+                url: urlapi + 'conversations/' + $scope.selectedConversation._id,
+                method: "POST",
+                data: $scope.newMessage
+            })
             .then(function (data) {
+                  console.log(data);
+                    //toastr.success('message sent');
+                    $scope.conversations = data.data; // for UI
+                    $scope.selectedConversation=$scope.conversations[$scope.indexSelectedConversation];
+                    $scope.newMessage={};
+                },
+                function () {
+                    toastr.error('Failed on sending message');
+                });
+        };/* end of sendNewMessage */
+
+
+        $scope.conversations = {};
+        $scope.getConversations=function(){
+            $http.get(urlapi + 'conversations')
+            .then(function (data) {
+                console.log(Date());
                 console.log('data success');
                 console.log(data); // for browser console
                 $scope.conversations = data.data; // for UI
+                $scope.selectedConversation=$scope.conversations[$scope.indexSelectedConversation];
             }, function (data, status) {
                 console.log('data error');
                 console.log(status);
@@ -24,13 +56,20 @@ angular.module('myApp.messages', ['ngRoute', 'chart.js'])
             })
             .then(function (result) {
             });
+        };
 
-        $scope.orderByMe = function(x) {
-          $scope.orderBy = x;
-        };
-        $scope.selectedConversation={};
-        $scope.selectConversation=function(conv){
-            console.log(conv);
-            $scope.selectedConversation=conv;
-        };
+        if(window.location.hash=="#!/messages")
+        {
+            console.log("messages page");
+            $scope.timer = setInterval(function(){
+                if(window.location.hash=="#!/messages")
+                {
+                    $scope.getConversations();
+                }//s'ha d'acavar d'afinar això, ara cada cop que entres a la pàgina de messages, s'afegeix un nou timer als que ja teniem
+            }, 1000);//cada tres segons
+        }else{
+            console.log("not messages page");
+        }
+
+        $scope.getConversations();
     });
