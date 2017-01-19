9a9f479b744dc2ea84039de21878e7f611f643cf
lima9 <max9@openmailbox.org>
2017-01-20 00:10:16 +0100

    Añadidos contact a la base de datos

--- app/landingpage.js
+++ app/landingpage.js
@@ -1,26 +1,66 @@
-
 var urlapi = "http://147.83.7.158:3005/api/";
-
-
 var app = angular.module('landingpageApp', []);
-app.controller('landingpageCtrl', function($scope, $http) {
-
-  console.log("landingpage");
-  $scope.trainers = {};
-  $http.get(urlapi + 'trainers')
-      .success(function (data) {
-          console.log('data success');
-          console.log(data); // for browser console
-          $scope.trainers = data; // for UI
-          localStorage.setItem('fs_web_trainers', JSON.stringify($scope.trainers));
-      })
-      .error(function (data, status) {
-          console.log('data error');
-          console.log(status);
-          console.log(data);
-      })
-      .then(function (result) {
-          //users = result.data;
-      });
-
+app.controller('landingpageCtrl', function ($scope, $http) {
+    console.log("landingpage");
+    $scope.trainers = {};
+    $http.get(urlapi + 'trainers')
+        .success(function (data) {
+            console.log('data success');
+            console.log(data);
+            $scope.trainers = data;
+            localStorage.setItem('fs_web_trainers', JSON.stringify($scope.trainers));
+        })
+        .error(function (data, status) {
+            console.log('data error');
+            console.log(status);
+            console.log(data);
+        })
+        .then(function (result) {
+        });
+    $http.get(urlapi + 'chefs')
+        .success(function (data) {
+            console.log('data success');
+            console.log(data);
+            $scope.trainers = data;
+            localStorage.setItem('fs_web_chefs', JSON.stringify($scope.chefs));
+        })
+        .error(function (data, status) {
+            console.log('data error');
+            console.log(status);
+            console.log(data);
+        })
+        .then(function (result) {
+        });
+    $http.get(urlapi + 'diets')
+        .success(function (data) {
+            console.log('data success');
+            console.log(data); // for browser console
+            $scope.diets = data; // for UI
+            localStorage.setItem('fs_web_diets', JSON.stringify($scope.diets));
+        })
+        .error(function (data, status) {
+            console.log('data error');
+            console.log(status);
+            console.log(data);
+        })
+        .then(function (result) {
+        });
+    $scope.giveInfo = function () {
+        console.log('Giving Contact Info', $scope.contactdata);
+        $http({
+            url: urlapi + '/contacts',
+            method: "POST",
+            data: $scope.contactdata
+        })
+            .then(function (response) {
+                    console.log("response: ");
+                    console.log(response.data);
+                    if (response.data.success == true) {
+                        window.location.reload();
+                    } else {
+                        console.log("Giving Contact Info failed");
+                    }
+                }
+            )
+    }
 });
