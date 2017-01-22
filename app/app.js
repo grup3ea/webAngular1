cb349130e1e86f827f5d08e92a83538190d92199
arnaucode <idoctnef@openmailbox.org>
2017-01-22 17:39:12 +0100

    production

--- app/app.js
+++ app/app.js
@@ -1,9 +1,9 @@
 'use strict';
 
 
-var urlapi = "http://localhost:3005/api/";
+//var urlapi = "http://localhost:3005/api/";
 //var urlapi = "http://192.168.1.34:3005/api/";
-//var urlapi="http://147.83.7.158:3005/api/";
+var urlapi="http://147.83.7.158:3005/api/";
 
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
